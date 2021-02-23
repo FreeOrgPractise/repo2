62dcbbac19af89ec2fae743aa67f6fcf7ff60e6a
pradeep0990 <76524964+pradeep0990@users.noreply.github.com>
2021-02-22 18:26:29 -0600

    Update LoginServlet.java

--- src/main/java/ApplicationController/LoginServlet.java
+++ src/main/java/ApplicationController/LoginServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// this is a login servelet.
+
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
